1a1f34661e007d7dc87622bdb2d7dfdf0ccc3d14
Serdar Ozturk <info@fogosoft.co.uk>
2024-09-26 11:02:08 +0100

    Get updated conversation details
    We need to call conversation() free function in the callback
        to get updated conversation data and messages
    
    ET-1239

---
+++ mail-message/data-rust/src/main/kotlin/ch/protonmail/android/mailmessage/data/usecase/GetRustConversationMessages.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmessage.data.usecase
+
+import ch.protonmail.android.mailcommon.datarust.mapper.LocalConversationId
+import uniffi.proton_mail_uniffi.ConversationAndMessages
+import uniffi.proton_mail_uniffi.Mailbox
+import uniffi.proton_mail_uniffi.conversation
+import javax.inject.Inject
+
+class GetRustConversationMessages @Inject constructor() {
+
+    suspend operator fun invoke(mailbox: Mailbox, conversationId: LocalConversationId): ConversationAndMessages? =
+        conversation(mailbox, conversationId)
+}
