fd70b99d9efd1bdb55f0b2e03d70006498a24754
Marino Meneghel <marino.meneghel@protonmail.com>
2024-10-22 18:08:28 +0200

    Add use cases to get all bottom bar actions for message and convo
    ET-1333

---
+++ mail-conversation/domain/src/test/kotlin/ch/protonmail/android/mailconversation/domain/usecase/GetAllConversationBottomBarActionsTest.kt
@@ -0,0 +1,60 @@
+package ch.protonmail.android.mailconversation.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.Action
+import ch.protonmail.android.mailcommon.domain.model.AllBottomBarActions
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.sample.ConversationIdSample
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailconversation.domain.repository.ConversationActionRepository
+import ch.protonmail.android.maillabel.domain.sample.LabelIdSample
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class GetAllConversationBottomBarActionsTest {
+
+    private val actionRepository = mockk<ConversationActionRepository>()
+
+    private val getAllConversationBottomBarActions = GetAllConversationBottomBarActions(actionRepository)
+
+    @Test
+    fun `returns available actions when repo succeeds`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val labelId = LabelIdSample.Trash
+        val conversationIds = listOf(ConversationIdSample.Newsletter)
+        val expected = AllBottomBarActions(
+            listOf(Action.MarkRead, Action.MarkUnread),
+            listOf(Action.Star, Action.Label)
+        )
+        coEvery {
+            actionRepository.getAllBottomBarActions(userId, labelId, conversationIds)
+        } returns expected.right()
+
+        // When
+        val actual = getAllConversationBottomBarActions(userId, labelId, conversationIds)
+
+        // Then
+        assertEquals(expected.right(), actual)
+    }
+
+    @Test
+    fun `returns error when repository fails to get available actions`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val labelId = LabelIdSample.Trash
+        val conversationIds = listOf(ConversationIdSample.Newsletter)
+        val expected = DataError.Local.Unknown.left()
+        coEvery { actionRepository.getAllBottomBarActions(userId, labelId, conversationIds) } returns expected
+
+        // When
+        val actual = getAllConversationBottomBarActions(userId, labelId, conversationIds)
+
+        // Then
+        assertEquals(expected, actual)
+    }
+}
