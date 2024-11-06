d87de99d579a399a40af427253e8bac9fa6f6ab2
Serdar Ozturk <info@fogosoft.co.uk>
2024-11-06 13:37:22 +0000

    Material 3 - Medium Top App Bar
    Material 3 defines different top app bar types. In contact list
        screen we will need a larger top app bar, therefore
        MediumTtopAppBar added.
    
    ET-1317

--- presentation-compose/src/main/java/ch/protonmail/android/design/compose/component/appbar/ProtonTopAppBar.kt
+++ presentation-compose/src/main/java/ch/protonmail/android/design/compose/component/appbar/ProtonTopAppBar.kt
@@ -1,28 +1,29 @@
 /*
- * Copyright (c) 2021 Proton Technologies AG
- * This file is part of Proton Technologies AG and ProtonMail.
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
  *
- * ProtonMail is free software: you can redistribute it and/or modify
+ * Proton Mail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ProtonMail is distributed in the hope that it will be useful,
+ * Proton Mail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ProtonMail.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package ch.protonmail.android.design.compose.component.appbar
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
@@ -58,6 +59,31 @@ fun ProtonTopAppBar(
 }
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ProtonMediumTopAppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit),
+    actions: @Composable RowScope.() -> Unit = {},
+    backgroundColor: Color = ProtonTheme.colors.backgroundNorm,
+    contentColor: Color = ProtonTheme.colors.textNorm
+) {
+    MediumTopAppBar(
+        title = title,
+        modifier = modifier,
+        navigationIcon = navigationIcon,
+        actions = actions,
+        colors = topAppBarColors(
+            containerColor = backgroundColor,
+            scrolledContainerColor = backgroundColor,
+            navigationIconContentColor = contentColor,
+            titleContentColor = contentColor,
+            actionIconContentColor = contentColor
+        )
+    )
+}
+
+@Composable
 @Preview(showSystemUi = true)
 fun ProtonTopAppBarPreview() {
     ProtonTheme {
@@ -67,7 +93,7 @@ fun ProtonTopAppBarPreview() {
             },
             navigationIcon = {
                 Icon(
-                    imageVector = Icons.Filled.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = "Back",
                     tint = Color.White
                 )
