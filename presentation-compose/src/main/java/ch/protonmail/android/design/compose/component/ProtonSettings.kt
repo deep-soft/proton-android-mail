c2fb0d7f3ea95d88dc17ae2a29884b1979475735
Serdar Ozturk <info@fogosoft.co.uk>
2024-12-04 22:16:50 +0000

    Proton main settings item composables
    Implemented main settings composable items in order to
       use in main settings page
    
    ET-1537

--- presentation-compose/src/main/java/ch/protonmail/android/design/compose/component/ProtonSettings.kt
+++ presentation-compose/src/main/java/ch/protonmail/android/design/compose/component/ProtonSettings.kt
@@ -20,14 +20,18 @@ package ch.protonmail.android.design.compose.component
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
@@ -46,6 +50,8 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.semantics
@@ -56,6 +62,7 @@ import ch.protonmail.android.design.compose.component.appbar.ProtonTopAppBar
 import ch.protonmail.android.design.compose.theme.ProtonDimens
 import ch.protonmail.android.design.compose.theme.ProtonTheme
 import ch.protonmail.android.design.compose.theme.bodyLargeNorm
+import ch.protonmail.android.design.compose.theme.bodyLargeWeak
 import ch.protonmail.android.design.compose.theme.bodyMediumNorm
 import ch.protonmail.android.design.compose.theme.textNorm
 
@@ -116,12 +123,104 @@ fun ProtonSettingsHeader(modifier: Modifier = Modifier, title: String) {
         isClickable = false
     ) {
         Text(
-            modifier = Modifier.align(Alignment.Bottom),
+            modifier = Modifier.align(Alignment.CenterVertically),
             text = title,
-            color = ProtonTheme.colors.brandNorm,
+            color = ProtonTheme.colors.textWeak,
             style = ProtonTheme.typography.titleMedium
         )
     }
+}
+
+@Composable
+fun ProtonMainSettingsItem(
+    modifier: Modifier = Modifier,
+    name: String,
+    @DrawableRes iconRes: Int,
+    hint: String,
+    isClickable: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+    ProtonMainSettingsItem(
+        modifier = modifier,
+        name = name,
+        hint = hint,
+        icon = {
+            ProtonMainSettingsIcon(
+                iconRes = iconRes,
+                contentDescription = "Sample Icon",
+                tint = ProtonTheme.colors.textNorm
+            )
+        },
+        isClickable = isClickable,
+        onClick = onClick
+    )
+}
+
+@Composable
+fun ProtonMainSettingsItem(
+    modifier: Modifier = Modifier,
+    name: String,
+    icon: @Composable () -> Unit,
+    hint: String,
+    isClickable: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(isClickable, onClick = onClick)
+            .padding(ProtonDimens.Spacing.Large),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(ProtonDimens.DefaultIconWithPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            icon()
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(start = ProtonDimens.Spacing.Large)
+                .semantics(mergeDescendants = true) {}
+                .weight(1f)
+        ) {
+            Text(
+                modifier = Modifier,
+                text = name,
+                style = ProtonTheme.typography.bodyLargeWeak
+            )
+            Text(
+                modifier = Modifier.padding(top = ProtonDimens.Spacing.Small),
+                text = hint,
+                color = ProtonTheme.colors.textHint,
+                style = ProtonTheme.typography.bodyMedium
+            )
+
+        }
+
+        Icon(
+            modifier = Modifier
+                .padding(start = ProtonDimens.Spacing.Large),
+            painter = painterResource(id = R.drawable.ic_proton_chevron_right),
+            contentDescription = hint,
+            tint = ProtonTheme.colors.iconDisabled
+        )
+    }
+}
+
+@Composable
+fun ProtonMainSettingsIcon(
+    @DrawableRes iconRes: Int,
+    contentDescription: String,
+    tint: Color = ProtonTheme.colors.textNorm
+) {
+    Icon(
+        painter = painterResource(id = iconRes),
+        contentDescription = contentDescription,
+        tint = tint
+    )
 }
 
 @Composable
