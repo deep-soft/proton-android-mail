0f095bac06e2bd8ed73ea357b25a4e756fa6ee57
Marino Meneghel <marino.meneghel@protonmail.com>
2024-12-06 14:40:22 +0100

    Add Rust Mailbox Factory use case to allow building instances of rust mailbox
    ET-1689

---
+++ mail-message/data-rust/src/main/kotlin/ch/protonmail/android/mailmessage/data/local/RustMailboxFactory.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmessage.data.local
+
+import arrow.core.Either
+import arrow.core.right
+import ch.protonmail.android.mailcommon.datarust.mapper.LocalLabelId
+import ch.protonmail.android.mailcommon.datarust.usecase.ExecuteWithUserSession
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailmessage.data.usecase.CreateAllMailMailbox
+import ch.protonmail.android.mailmessage.data.usecase.CreateMailbox
+import ch.protonmail.android.mailmessage.data.wrapper.MailboxWrapper
+import me.proton.core.domain.entity.UserId
+import timber.log.Timber
+import javax.inject.Inject
+
+class RustMailboxFactory @Inject constructor(
+    private val executeWithUserSession: ExecuteWithUserSession,
+    private val createMailbox: CreateMailbox,
+    private val createAllMailMailbox: CreateAllMailMailbox
+) {
+
+    private val mailboxCache: MutableMap<LocalLabelId, MailboxWrapper> = mutableMapOf()
+
+    suspend fun create(userId: UserId, labelId: LocalLabelId): Either<DataError, MailboxWrapper> {
+        val cachedMailbox = mailboxCache[labelId]
+        if (cachedMailbox != null) {
+            return cachedMailbox.right()
+        }
+
+        return executeWithUserSession(userId) { session ->
+            val mailbox = createMailbox(session, labelId)
+            mailboxCache[labelId] = mailbox
+            Timber.d("rust-mailbox-factory: Mailbox created for label: $labelId")
+            mailbox
+        }
+    }
+
+    suspend fun createAllMail(userId: UserId): Either<DataError, MailboxWrapper> =
+        executeWithUserSession(userId) { session ->
+            Timber.d("rust-mailbox-factory: Mailbox created for all mail label")
+            createAllMailMailbox(session)
+        }
+
+}
