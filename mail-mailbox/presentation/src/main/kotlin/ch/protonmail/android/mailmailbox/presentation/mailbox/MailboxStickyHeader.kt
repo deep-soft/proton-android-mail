d931d28b99d8306a43568592acbb3e814cc5edf0
Serdar Ozturk <info@fogosoft.co.uk>
2024-12-18 20:10:34 +0000

    Updated mailbox sticky header composable
    Extracted the function to a separate composable and added select
    all & deselect all functionality. Defined actions including unread
    filter and select all / deselect all
    
    ET-1508

---
+++ mail-mailbox/presentation/src/main/kotlin/ch/protonmail/android/mailmailbox/presentation/mailbox/MailboxStickyHeader.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.presentation.mailbox
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ch.protonmail.android.design.compose.theme.ProtonDimens
+import ch.protonmail.android.mailmailbox.presentation.mailbox.model.MailboxListState
+import ch.protonmail.android.mailmailbox.presentation.mailbox.model.MailboxState
+
+@Composable
+fun MailboxStickyHeader(
+    modifier: Modifier = Modifier,
+    state: MailboxState,
+    actions: MailboxStickyHeader.Actions
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = ProtonDimens.Spacing.Large),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        if (state.mailboxListState is MailboxListState.Data.SelectionMode) {
+            SelectDeselectAllButton(
+                areAllItemsSelected = state.mailboxListState.areAllItemsSelected,
+                actions = SelectDeselectAllButton.Actions(
+                    onSelectAllClicked = actions.onSelectAllClicked,
+                    onDeselectAllClicked = actions.onDeselectAllClicked
+                )
+            )
+
+        } else {
+            UnreadItemsFilter(
+                modifier = Modifier,
+                state = state.unreadFilterState,
+                onFilterEnabled = actions.onUnreadFilterEnabled,
+                onFilterDisabled = actions.onUnreadFilterDisabled
+            )
+        }
+
+    }
+}
+
+object MailboxStickyHeader {
+    data class Actions(
+        val onUnreadFilterEnabled: () -> Unit,
+        val onUnreadFilterDisabled: () -> Unit,
+        val onSelectAllClicked: () -> Unit,
+        val onDeselectAllClicked: () -> Unit
+    ) {
+
+        companion object {
+
+            val Empty = Actions(
+                onUnreadFilterEnabled = {},
+                onUnreadFilterDisabled = {},
+                onSelectAllClicked = {},
+                onDeselectAllClicked = {}
+            )
+        }
+    }
+}
