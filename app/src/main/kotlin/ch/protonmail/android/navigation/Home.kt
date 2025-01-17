85a95060366bfe283381743c90a5d7a2d5a4a4b1
Marino Meneghel <marino.meneghel@protonmail.com>
2025-01-17 16:15:17 +0100

    Remove corner radious from sidebar
    ET-1880

--- app/src/main/kotlin/ch/protonmail/android/navigation/Home.kt
+++ app/src/main/kotlin/ch/protonmail/android/navigation/Home.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -271,7 +272,8 @@ fun Home(
                 windowInsets = WindowInsets(
                     top = ProtonDimens.Spacing.ExtraLarge,
                     bottom = ProtonDimens.Spacing.ExtraLarge
-                )
+                ),
+                drawerShape = RectangleShape
             ) {
                 Sidebar(
                     drawerState = drawerState,
