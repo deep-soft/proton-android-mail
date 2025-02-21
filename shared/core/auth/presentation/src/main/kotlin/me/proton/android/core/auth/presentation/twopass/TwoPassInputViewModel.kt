d06e1352c8a44b1d006e4854c5686e597b7d4463
Marino Meneghel <marino.meneghel@protonmail.com>
2025-02-21 12:41:47 +0100

    Fix Login with Mailbox Password failure due to state not being emitted
    This ensures the state gerated by onWaitFinished method is emitted by wrapping
    the call in an emitAll() and adds handliing for error in the initialize() phase.
    
    ET-2190

--- shared/core/auth/presentation/src/main/kotlin/me/proton/android/core/auth/presentation/twopass/TwoPassInputViewModel.kt
+++ shared/core/auth/presentation/src/main/kotlin/me/proton/android/core/auth/presentation/twopass/TwoPassInputViewModel.kt
@@ -53,6 +53,7 @@ import uniffi.proton_mail_uniffi.MailUserSession
 import uniffi.proton_mail_uniffi.StoredAccount
 import uniffi.proton_mail_uniffi.StoredSession
 import uniffi.proton_mail_uniffi.VoidLoginResult
+import uniffi.proton_mail_uniffi.VoidSessionResult
 import javax.inject.Inject
 
 @HiltViewModel
@@ -120,14 +121,18 @@ class TwoPassInputViewModel @Inject constructor(
     private fun onSuccess(loginFlow: LoginFlow): Flow<TwoPassInputState> = flow {
         when (val result = loginFlow.toUserContext()) {
             is LoginFlowToUserContextResult.Error -> emitAll(onError(result.v1))
-            is LoginFlowToUserContextResult.Ok -> onWaitFinished(result.v1)
+            is LoginFlowToUserContextResult.Ok -> emitAll(onWaitFinished(result.v1))
         }
     }
 
     private fun onWaitFinished(mailUserSession: MailUserSession) = flow {
-        mailUserSession.initialize(callback)
-        callback.waitFinished()
-        emit(TwoPassInputState.Success)
+        when (val result = mailUserSession.initialize(callback)) {
+            is VoidSessionResult.Error -> emit(Error.LoginFlow(result.v1.toString()))
+            VoidSessionResult.Ok -> {
+                callback.waitFinished()
+                emit(TwoPassInputState.Success)
+            }
+        }
     }
 
     private suspend fun getSession(account: StoredAccount?): List<StoredSession>? {
