19969aa140364a6cfe175d67e0fc6870f41ed3dd
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-02-26 14:49:10 +0100

    Add additional LogsActivity for troubleshooting
    ET-2229

---
+++ app/src/alpha/kotlin/ch/protonmail/android/debug/LogsActivity.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.debug
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ch.protonmail.android.design.compose.theme.ProtonTheme
+import ch.protonmail.android.mailbugreport.presentation.ui.ApplicationLogsPeekView
+import ch.protonmail.android.mailbugreport.presentation.ui.ApplicationLogsScreen
+import ch.protonmail.android.mailcommon.presentation.extension.navigateBack
+import ch.protonmail.android.navigation.model.Destination.Screen
+import dagger.hilt.android.AndroidEntryPoint
+import io.sentry.compose.withSentryObservableEffect
+
+@AndroidEntryPoint
+internal class LogsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ProtonTheme {
+                val navController = rememberNavController().withSentryObservableEffect()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.ApplicationLogs.route
+                ) {
+                    composable(route = Screen.ApplicationLogs.route) {
+                        ApplicationLogsScreen(
+                            onBackClick = { finish() },
+                            onViewItemClick = { navController.navigate(Screen.ApplicationLogsView(it)) }
+                        )
+                    }
+                    composable(route = Screen.ApplicationLogsView.route) {
+                        ApplicationLogsPeekView(
+                            onBack = { navController.navigateBack() }
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
