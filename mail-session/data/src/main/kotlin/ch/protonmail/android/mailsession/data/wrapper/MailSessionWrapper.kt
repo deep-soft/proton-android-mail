728e39ad77ecc527519471461056ed0abf62a2e5
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-04-22 11:45:39 +0200

    Add startBackgroundExecution to MailSessionWrapper
    ET-2760

--- mail-session/data/src/main/kotlin/ch/protonmail/android/mailsession/data/wrapper/MailSessionWrapper.kt
+++ mail-session/data/src/main/kotlin/ch/protonmail/android/mailsession/data/wrapper/MailSessionWrapper.kt
@@ -25,6 +25,7 @@ import ch.protonmail.android.mailcommon.data.mapper.LocalUserId
 import ch.protonmail.android.mailcommon.data.mapper.toDataError
 import ch.protonmail.android.mailcommon.domain.model.DataError
 import ch.protonmail.android.mailsession.domain.wrapper.MailUserSessionWrapper
+import uniffi.proton_mail_uniffi.BackgroundExecutionCallback
 import uniffi.proton_mail_uniffi.LiveQueryCallback
 import uniffi.proton_mail_uniffi.MailSession
 import uniffi.proton_mail_uniffi.MailSessionGetAccountResult
@@ -99,4 +100,6 @@ class MailSessionWrapper(private val mailSession: MailSession) {
     suspend fun setPrimaryAccount(userId: LocalUserId) = mailSession.setPrimaryAccount(userId)
 
     fun registerDeviceTask() = mailSession.registerDeviceTask()
+
+    fun startBackgroundTask(callback: BackgroundExecutionCallback) = mailSession.startBackgroundExecution(callback)
 }
