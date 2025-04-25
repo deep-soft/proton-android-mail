14491675aa28f7725be19f2d347b90eea6b879e7
Serdar Ozturk <info@fogosoft.co.uk>
2025-04-25 14:01:44 +0100

    Implemented use case to observe system mail labels
    ET-2448

---
+++ mail-label/domain/src/main/kotlin/ch/protonmail/android/maillabel/domain/usecase/ObserveSystemMailLabels.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.maillabel.domain.usecase
+
+import arrow.core.Either
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.maillabel.domain.model.MailLabel
+import ch.protonmail.android.maillabel.domain.model.toDynamicSystemMailLabel
+import ch.protonmail.android.maillabel.domain.repository.LabelRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+
+class ObserveSystemMailLabels @Inject constructor(
+    private val labelRepository: LabelRepository
+) {
+
+    operator fun invoke(userId: UserId): Flow<Either<DataError, List<MailLabel.System>>> =
+        labelRepository.observeSystemLabels(userId).mapLatest { labels ->
+            labels
+                .toDynamicSystemMailLabel()
+                .right()
+        }
+}
