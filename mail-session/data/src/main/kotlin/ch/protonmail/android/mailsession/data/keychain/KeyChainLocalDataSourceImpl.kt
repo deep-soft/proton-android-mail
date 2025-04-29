d962f39350bd89d77feb7b64610d1f5733bfb083
Marino Meneghel <marino.meneghel@protonmail.com>
2025-04-29 15:15:35 +0200

    Add key chain data source impl based on data store
    ET-806

---
+++ mail-session/data/src/main/kotlin/ch/protonmail/android/mailsession/data/keychain/KeyChainLocalDataSourceImpl.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2025 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsession.data.keychain
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.data.mapper.safeData
+import ch.protonmail.android.mailcommon.data.mapper.safeEdit
+import ch.protonmail.android.mailcommon.domain.model.PreferencesError
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import me.proton.android.core.crypto.domain.model.EncryptedString
+import uniffi.proton_mail_uniffi.OsKeyChainEntryKind
+import javax.inject.Inject
+
+class KeyChainLocalDataSourceImpl @Inject constructor(
+    private val dataStoreProvider: KeyChainDataStoreProvider
+) : KeyChainLocalDataSource {
+
+    override suspend fun get(key: OsKeyChainEntryKind): Either<PreferencesError, EncryptedString> {
+        val preferenceKey = stringPreferencesKey(key.name)
+
+        val encryptedSecret = dataStoreProvider.keyChainDataStore.safeData.map { prefsEither ->
+            prefsEither.map { prefs -> prefs[preferenceKey] }
+        }
+            .first()
+            .getOrNull()
+
+        if (encryptedSecret == null) {
+            return PreferencesError.left()
+        }
+
+        return encryptedSecret.right()
+    }
+
+    override suspend fun save(key: OsKeyChainEntryKind, value: EncryptedString): Either<PreferencesError, Unit> {
+        val preferenceKey = stringPreferencesKey(key.name)
+
+        return dataStoreProvider.keyChainDataStore.safeEdit { mutablePreferences ->
+            mutablePreferences[preferenceKey] = value
+        }.map { }
+    }
+
+    override suspend fun remove(key: OsKeyChainEntryKind): Either<PreferencesError, Unit> {
+        val preferenceKey = stringPreferencesKey(key.name)
+
+        return dataStoreProvider.keyChainDataStore.safeEdit { it.remove(preferenceKey) }
+            .map { }
+    }
+}
