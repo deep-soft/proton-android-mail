eeb8634cd17b7550ddc2c362134fb47b2fdf5f7e
Neil Marietta <neil.marietta@protonmail.com>
2025-05-06 11:53:20 +0200

    Upgraded Core to 32.0.1.

--- app/src/main/kotlin/ch/protonmail/android/db/AppDatabase.kt
+++ app/src/main/kotlin/ch/protonmail/android/db/AppDatabase.kt
@@ -184,7 +184,7 @@ abstract class AppDatabase :
     companion object {
 
         const val name = "db-mail"
-        const val version = 40
+        const val version = 41
 
         internal val migrations = listOf(
             AppDatabaseMigrations.MIGRATION_1_2,
@@ -225,7 +225,8 @@ abstract class AppDatabase :
             AppDatabaseMigrations.MIGRATION_36_37,
             AppDatabaseMigrations.MIGRATION_37_38,
             AppDatabaseMigrations.MIGRATION_38_39,
-            AppDatabaseMigrations.MIGRATION_39_40
+            AppDatabaseMigrations.MIGRATION_39_40,
+            AppDatabaseMigrations.MIGRATION_40_41
         )
 
         fun buildDatabase(context: Context): AppDatabase = databaseBuilder<AppDatabase>(context, name)
