482a10b86ec0feed73657299a7b6a0f1bce73a0e
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-05-12 15:41:50 +0200

    Add RustBugReportDataSource + Repository
    ET-549

---
+++ mail-bugreport/data/src/main/kotlin/ch/protonmail/android/mailbugreport/data/local/RustBugReportDataSourceImpl.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailbugreport.data.local
+
+import arrow.core.raise.either
+import ch.protonmail.android.mailcommon.data.mapper.LocalIssueReport
+import ch.protonmail.android.mailcommon.data.mapper.toDataError
+import ch.protonmail.android.mailcommon.domain.coroutines.IODispatcher
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailsession.domain.repository.UserSessionRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import me.proton.core.domain.entity.UserId
+import uniffi.proton_mail_uniffi.VoidSessionResult
+import uniffi.proton_mail_uniffi.reportAnIssue
+import javax.inject.Inject
+
+class RustBugReportDataSourceImpl @Inject constructor(
+    private val userSessionRepository: UserSessionRepository,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher
+) : RustBugReportDataSource {
+
+    override suspend fun submitBugReport(userId: UserId, report: LocalIssueReport) = with(ioDispatcher) {
+        either {
+            val session = userSessionRepository.getUserSession(userId)?.getRustUserSession()
+                ?: raise(DataError.Local.NoUserSession)
+
+            val issue = reportAnIssue(session, report)
+
+            when (issue) {
+                is VoidSessionResult.Error -> raise(issue.v1.toDataError())
+                VoidSessionResult.Ok -> Unit
+            }
+        }
+    }
+}
---
+++ mail-bugreport/data/src/main/kotlin/ch/protonmail/android/mailbugreport/data/repository/BugReportRepositoryImpl.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailbugreport.data.repository
+
+import arrow.core.Either
+import ch.protonmail.android.mailbugreport.data.local.RustBugReportDataSource
+import ch.protonmail.android.mailbugreport.data.mapper.toLocalIssueReport
+import ch.protonmail.android.mailbugreport.domain.model.IssueReport
+import ch.protonmail.android.mailbugreport.domain.repository.BugReportRepository
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+
+class BugReportRepositoryImpl @Inject constructor(
+    private val bugReportDataSource: RustBugReportDataSource
+) : BugReportRepository {
+
+    override suspend fun submitBugReport(userId: UserId, issueReport: IssueReport): Either<DataError, Unit> {
+        val localIssueReport = issueReport.toLocalIssueReport()
+        return bugReportDataSource.submitBugReport(userId, localIssueReport)
+    }
+}
