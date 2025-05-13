2b3d77d6877e95c59979172b25a6b009856809d5
Marino Meneghel <marino.meneghel@protonmail.com>
2025-05-13 16:04:23 +0200

    Add use cases to add / remove inline attachments
    ET-2932

---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/AddInlineAttachment.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import android.net.Uri
+import arrow.core.Either
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcomposer.domain.model.AttachmentAddError
+import ch.protonmail.android.mailcomposer.domain.repository.AttachmentRepository
+import javax.inject.Inject
+
+class AddInlineAttachment @Inject constructor(
+    private val attachmentRepository: AttachmentRepository
+) {
+
+    suspend operator fun invoke(fileUri: Uri): Either<AttachmentAddError, Unit> =
+        attachmentRepository.addInlineAttachment(fileUri).mapLeft {
+            when (it) {
+                DataError.Local.AttachmentError.AttachmentTooLarge -> AttachmentAddError.AttachmentTooLarge
+                DataError.Local.AttachmentError.EncryptionError -> AttachmentAddError.EncryptionError
+                DataError.Local.AttachmentError.InvalidDraftMessage -> AttachmentAddError.InvalidDraftMessage
+                DataError.Local.AttachmentError.TooManyAttachments -> AttachmentAddError.TooManyAttachments
+                DataError.Local.Unknown -> AttachmentAddError.Unknown
+                else -> AttachmentAddError.Unknown
+            }
+        }
+
+}
---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/DeleteInlineAttachment.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.Either
+import arrow.core.raise.either
+import ch.protonmail.android.mailattachments.domain.model.AttachmentId
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcomposer.domain.model.AttachmentDeleteError
+import ch.protonmail.android.mailcomposer.domain.repository.AttachmentRepository
+import javax.inject.Inject
+
+class DeleteInlineAttachment @Inject constructor(
+    private val attachmentRepository: AttachmentRepository
+) {
+
+    suspend operator fun invoke(attachmentId: AttachmentId): Either<AttachmentDeleteError, Unit> = either {
+        attachmentRepository.deleteInlineAttachment(attachmentId)
+            .mapLeft {
+                when (it) {
+                    DataError.Local.FailedToDeleteFile -> AttachmentDeleteError.FailedToDeleteFile
+                    DataError.Local.AttachmentError.InvalidDraftMessage -> AttachmentDeleteError.InvalidDraftMessage
+                    else -> AttachmentDeleteError.Unknown
+                }
+            }
+            .bind()
+    }
+}
