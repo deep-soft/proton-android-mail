c3f02b972990fa9783d834a595cc415c3fa36bd0
Serdar Ozturk <info@fogosoft.co.uk>
2025-05-15 11:33:00 +0100

    Define migration error domain data model
    ET-2894

---
+++ mail-legacy-migration/domain/src/main/kotlin/ch/protonmail/android/legacymigration/domain/model/MigrationError.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.legacymigration.domain.model
+
+import ch.protonmail.android.mailcommon.domain.model.DataError
+
+sealed interface MigrationError {
+    sealed interface LegacyDbFailure {
+        data object MissingPrimaryUserId : MigrationError
+        data object MissingAccount : MigrationError
+        data object MissingSessionId : MigrationError
+        data object MissingSession : MigrationError
+        data object MissingUser : MigrationError
+        data object MissingUserAddress : MigrationError
+
+    }
+
+    data object LoginFlowFailed : MigrationError
+    data object Unknown : MigrationError
+
+    // Error during migration with specific known reason
+    sealed interface MigrateFailed : MigrationError {
+        data object InvalidCredentials : MigrateFailed
+        data object UnsupportedTwoFactorAuth : MigrateFailed
+        data object CantUnlockUserKey : MigrateFailed
+        data class Other(val error: DataError) : MigrateFailed
+    }
+}
