291765f8d118dd54718ac7bd04a96a630dafc675
Marino Meneghel <marino.meneghel@protonmail.com>
2025-06-03 15:58:44 +0200

    Add main and composite events and state modifications
    ET-3234

---
+++ mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/model/operations/ComposerCompositeEvents.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.presentation.model.operations
+
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.presentation.model.ComposerState
+import ch.protonmail.android.mailcomposer.presentation.model.DraftUiModel
+import ch.protonmail.android.mailcomposer.presentation.model.SenderUiModel
+import ch.protonmail.android.mailcomposer.presentation.reducer.modifications.ComposerStateModifications
+import ch.protonmail.android.mailcomposer.presentation.reducer.modifications.MainStateModification
+import kotlin.time.Duration
+
+internal sealed interface CompositeEvent : ComposerStateEvent {
+
+    override fun toStateModifications(): ComposerStateModifications = when (this) {
+        is DraftContentReady -> ComposerStateModifications(
+            mainModification = MainStateModification.OnDraftReady(draftUiModel)
+//            effectsModification = ContentEffectsStateModifications.DraftContentReady(
+//                senderValidationResult,
+//                isDataRefreshed,
+//                forceBodyFocus
+//            )
+        )
+
+        is SenderAddressesListReady -> ComposerStateModifications(
+            mainModification = MainStateModification.SendersListReady(sendersList)
+//            effectsModification = BottomSheetEffectsStateModification.ShowBottomSheet
+        )
+
+        is OnSendWithEmptySubject -> ComposerStateModifications(
+            mainModification = MainStateModification.UpdateLoading(ComposerState.LoadingType.None)
+//            effectsModification = ConfirmationsEffectsStateModification.SendNoSubjectConfirmationRequested
+        )
+
+        is SetExpirationDismissed -> ComposerStateModifications(
+//            effectsModification = BottomSheetEffectsStateModification.HideBottomSheet,
+//            accessoriesModification = AccessoriesStateModification.MessageExpirationUpdated(expiration)
+        )
+
+        is UserChangedSender -> ComposerStateModifications(
+            mainModification = MainStateModification.UpdateSender(newSender)
+//            effectsModification = BottomSheetEffectsStateModification.HideBottomSheet
+        )
+    }
+
+    // Replaces the existing PrefillDraftDataReceived
+    data class DraftContentReady(
+        val draftUiModel: DraftUiModel,
+        val isDataRefreshed: Boolean,
+        val bodyShouldTakeFocus: Boolean
+    ) : CompositeEvent
+
+    data class SenderAddressesListReady(val sendersList: List<SenderUiModel>) : CompositeEvent
+    data class UserChangedSender(val newSender: SenderEmail) : CompositeEvent
+
+    data class SetExpirationDismissed(val expiration: Duration) : CompositeEvent
+
+    data object OnSendWithEmptySubject : CompositeEvent
+}
---
+++ mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/reducer/modifications/MainStateModification.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.presentation.reducer.modifications
+
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.presentation.model.ComposerState
+import ch.protonmail.android.mailcomposer.presentation.model.DraftUiModel
+import ch.protonmail.android.mailcomposer.presentation.model.SenderUiModel
+import kotlinx.collections.immutable.toImmutableList
+
+internal sealed interface MainStateModification : ComposerStateModification<ComposerState.Main> {
+
+    data class OnDraftReady(val draftUiModel: DraftUiModel) : MainStateModification {
+
+        override fun apply(state: ComposerState.Main): ComposerState.Main = state.copy(
+            fields = state.fields.copy(
+                sender = SenderUiModel(draftUiModel.draftFields.sender.value),
+                body = draftUiModel.draftFields.body.value,
+                displayBody = draftUiModel.draftDisplayBodyUiModel
+            ),
+            loadingType = ComposerState.LoadingType.None
+        )
+    }
+
+    data class UpdateLoading(val value: ComposerState.LoadingType) : MainStateModification {
+
+        override fun apply(state: ComposerState.Main): ComposerState.Main = state.copy(loadingType = value)
+    }
+
+    data class UpdateSender(val sender: SenderEmail) : MainStateModification {
+
+        override fun apply(state: ComposerState.Main): ComposerState.Main = state.copy(
+            fields = state.fields.copy(sender = SenderUiModel(sender.value))
+        )
+    }
+
+    data class SendersListReady(val list: List<SenderUiModel>) : MainStateModification {
+
+        override fun apply(state: ComposerState.Main): ComposerState.Main =
+            state.copy(senderAddresses = list.toImmutableList())
+    }
+
+    data class UpdateSubmittable(val isSubmittable: Boolean) : MainStateModification {
+
+        override fun apply(state: ComposerState.Main): ComposerState.Main = state.copy(isSubmittable = isSubmittable)
+    }
+
+}
