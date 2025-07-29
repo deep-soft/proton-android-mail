64622f6ac931f515e0776b118cfd43668eda503b
Seren <seren.matthews@external.proton.ch>
2025-07-29 14:54:23 +0200

    Make Settings Scrollable ET-3969

--- mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/SettingsScreen.kt
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/SettingsScreen.kt
@@ -30,6 +30,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -124,6 +126,7 @@ fun MainSettingsScreen(
             modifier = modifier
                 .testTag(SettingsScreenTestTags.SettingsList)
                 .padding(contentPadding)
+                .verticalScroll(rememberScrollState())
         ) {
             AccountSettingsItem(
                 modifier = Modifier.testTag(SettingsScreenTestTags.AccountSettingsItem),
