6d9bcf46c3a03033bcec7d4c57ba99d39b2084ec
Yoann Goular <yoann.goular@proton.ch>
2025-08-12 16:23:57 +0200

    create UiEventFlow for one-time event

---
+++ shared/core/auth/presentation/src/main/kotlin/me/proton/android/core/auth/presentation/flow/UiEventFlow.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.android.core.auth.presentation.flow
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.AbstractFlow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.withContext
+
+class UiEventFlow<T> : AbstractFlow<T>() {
+
+    private val events = Channel<T>(capacity = BUFFER_CAPACITY, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+
+    override suspend fun collectSafely(collector: FlowCollector<T>) {
+        events.receiveAsFlow().collect {
+            collector.emit(it)
+        }
+    }
+
+    suspend fun emit(event: T) {
+        withContext(Dispatchers.Main.immediate) {
+            events.send(event)
+        }
+    }
+
+    private companion object {
+
+        private const val BUFFER_CAPACITY = 16
+    }
+}
