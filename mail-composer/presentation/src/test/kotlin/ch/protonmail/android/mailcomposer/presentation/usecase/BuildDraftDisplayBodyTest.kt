5b211bbe2d6ca6ab60b547a6aba1cd8f487f1cec
Marino Meneghel <marino.meneghel@protonmail.com>
2025-08-20 17:52:54 +0200

    Remove unsafe options from composer js content security policy
    Such options would disable the core benefits of a csp
    
    ET-4404

--- mail-composer/presentation/src/test/kotlin/ch/protonmail/android/mailcomposer/presentation/usecase/BuildDraftDisplayBodyTest.kt
+++ mail-composer/presentation/src/test/kotlin/ch/protonmail/android/mailcomposer/presentation/usecase/BuildDraftDisplayBodyTest.kt
@@ -40,7 +40,7 @@ class BuildDraftDisplayBodyTest {
                 <head>
                     <title>Proton HTML Editor</title>
                     <meta id="myViewport" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes">
-                    <meta id="myCSP" http-equiv="Content-Security-Policy" content="script-src 'unsafe-inline' 'unsafe-eval'">
+                    <meta id="myCSP" http-equiv="Content-Security-Policy">
                     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
                     <style>
                         $customCss
