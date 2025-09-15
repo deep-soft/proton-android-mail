45c24bda2b8f6f1f76d3120306f3267670c2e49f
Marino Meneghel <marino.meneghel@protonmail.com>
2025-09-15 17:35:51 +0200

    Display previously selected date or current date when opening expiration date picker
    ET-4311

--- mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/ui/ExpirationTimeOptionsDialog.kt
+++ mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/ui/ExpirationTimeOptionsDialog.kt
@@ -18,6 +18,8 @@
 
 package ch.protonmail.android.mailcomposer.presentation.ui
 
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,10 +28,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import ch.protonmail.android.design.compose.component.ProtonAlertDialog
@@ -37,10 +42,15 @@ import ch.protonmail.android.design.compose.component.ProtonAlertDialogText
 import ch.protonmail.android.design.compose.component.ProtonDialogTitle
 import ch.protonmail.android.design.compose.component.ProtonRawListItem
 import ch.protonmail.android.design.compose.theme.ProtonDimens
+import ch.protonmail.android.design.compose.theme.ProtonTheme
 import ch.protonmail.android.mailcommon.presentation.ui.isLandscape
 import ch.protonmail.android.mailcomposer.presentation.R
 import ch.protonmail.android.mailcomposer.presentation.model.ExpirationTimeOption
 import ch.protonmail.android.mailcomposer.presentation.model.ExpirationTimeUiModel
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaZoneId
+import kotlin.time.Instant
+import kotlin.time.toJavaInstant
 
 @Composable
 fun ExpirationTimeOptionsDialog(
@@ -57,7 +67,7 @@ fun ExpirationTimeOptionsDialog(
                 ExpirationTimeOption.entries.forEach { item ->
                     SelectableExpirationTimeItem(
                         item = item,
-                        isSelected = selectedItem.selectedOption == item,
+                        selectedItem = selectedItem,
                         onSelected = { onTimePicked(it) }
                     )
                 }
@@ -82,10 +92,11 @@ private fun ExpirationTimeTitle() {
 @Composable
 private fun SelectableExpirationTimeItem(
     item: ExpirationTimeOption,
-    isSelected: Boolean,
+    selectedItem: ExpirationTimeUiModel,
     onSelected: (ExpirationTimeUiModel) -> Unit
 ) {
 
+    val isSelected = remember { item == selectedItem.selectedOption }
     ProtonRawListItem(
         modifier = Modifier
             .selectable(selected = isSelected) { onSelected(ExpirationTimeUiModel(item)) }
@@ -96,13 +107,51 @@ private fun SelectableExpirationTimeItem(
             selected = isSelected,
             onClick = null
         )
+        val isCustomTimeOption = item == ExpirationTimeOption.Custom
+        val isCustomTimeOptionSet = isCustomTimeOption && selectedItem.customTime != null
+
+        val text = when {
+            isCustomTimeOptionSet -> {
+                val formattedDate = selectedItem.customTime.formatDate()
+                stringResource(R.string.composer_expiration_time_custom_time_on, formattedDate)
+            }
+
+            else -> stringResource(id = item.textResId)
+        }
+
         Text(
             modifier = Modifier
                 .padding(start = ProtonDimens.Spacing.Large)
                 .weight(1f),
-            text = stringResource(id = item.textResId),
+            text = text,
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
+
+        when {
+            isCustomTimeOptionSet -> Icon(
+                modifier = Modifier.size(ProtonDimens.IconSize.Small),
+                painter = painterResource(id = R.drawable.ic_proton_pencil),
+                contentDescription = null,
+                tint = ProtonTheme.colors.iconNorm
+            )
+
+            isCustomTimeOption -> Icon(
+                modifier = Modifier.size(ProtonDimens.IconSize.Small),
+                painter = painterResource(id = R.drawable.ic_proton_chevron_right_filled),
+                contentDescription = null,
+                tint = ProtonTheme.colors.iconNorm
+
+            )
+        }
     }
+}
+
+@Composable
+private fun Instant.formatDate(): String {
+    val zone = TimeZone.currentSystemDefault()
+    val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        .withZone(zone.toJavaZoneId())
+
+    return formatter.format(this.toJavaInstant())
 }
