5813619f6683279ab0ce636c9cbc7f1ee672e8c1
Marino Meneghel <marino.meneghel@protonmail.com>
2025-11-07 17:42:52 +0100

    Make the account switcher bottomsheet full screen
    ET-5291

--- shared/core/account-manager/presentation/src/main/kotlin/me/proton/android/core/accountmanager/presentation/switcher/v2/AccountsSwitcherBottomSheetScreen.kt
+++ shared/core/account-manager/presentation/src/main/kotlin/me/proton/android/core/accountmanager/presentation/switcher/v2/AccountsSwitcherBottomSheetScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
@@ -96,7 +97,7 @@ fun AccountsSwitcherBottomSheetScreen(
     onEvent: (AccountSwitchEvent) -> Unit = {}
 ) {
     Box(
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     ) {
         when (state) {
             is AccountsManagerState.Loading -> ProtonHorizontallyCenteredProgress()
