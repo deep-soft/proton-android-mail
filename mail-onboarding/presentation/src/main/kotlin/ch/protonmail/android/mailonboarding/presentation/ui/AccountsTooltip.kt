f3a9e40b5effb82ab2f161cb47421ef49ee6b4da
Marino Meneghel <marino.meneghel@protonmail.com>
2025-11-13 10:29:05 +0100

    Dismiss 'accounts tooltip' box when clicking outside of it
    ET-5291

--- mail-onboarding/presentation/src/main/kotlin/ch/protonmail/android/mailonboarding/presentation/ui/AccountsTooltip.kt
+++ mail-onboarding/presentation/src/main/kotlin/ch/protonmail/android/mailonboarding/presentation/ui/AccountsTooltip.kt
@@ -26,15 +26,19 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.res.painterResource
@@ -43,6 +47,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import ch.protonmail.android.design.compose.theme.ProtonDimens
 import ch.protonmail.android.design.compose.theme.ProtonTheme
 import ch.protonmail.android.design.compose.theme.bodyMediumWeak
@@ -55,36 +61,39 @@ fun AccountsTooltip(
     onDismiss: () -> Unit
 ) {
 
-    TooltipBox(modifier, anchorBounds) {
-        Row(
-            modifier = Modifier
-                .clickable(enabled = false, onClick = {})
-                .fillMaxWidth()
-        ) {
+    val offsetY = anchorBounds?.bottom ?: 0f
 
-            SparkleIcon()
+    Popup(
+        alignment = Alignment.TopStart,
+        offset = IntOffset(x = 0, y = offsetY.toInt()),
+        onDismissRequest = onDismiss,
+        properties = PopupProperties(focusable = false)
+    ) {
+        TooltipBox(modifier) {
+            Row(
+                modifier = Modifier
+                    .clickable(enabled = false, onClick = {})
+                    .fillMaxWidth()
+            ) {
 
-            ToolTipText(Modifier.weight(2f, true))
+                SparkleIcon()
 
-            CloseIcon(onClose = onDismiss)
+                ToolTipText(Modifier.weight(2f, true))
+
+                CloseIcon(onClose = onDismiss)
+            }
         }
     }
 }
 
 @Composable
-private fun TooltipBox(
-    modifier: Modifier = Modifier,
-    anchorBounds: Rect?,
-    content: @Composable BoxScope.() -> Unit
-) {
-    val offsetY = anchorBounds?.bottom ?: 0f
+private fun TooltipBox(modifier: Modifier = Modifier, content: @Composable BoxScope.() -> Unit) {
     val tooltipShape = TooltipShape(offset = 120)
-
     Box(
         modifier = modifier
             .padding(ProtonDimens.Spacing.Standard)
             .fillMaxWidth()
-            .offset { IntOffset(0, offsetY.toInt()) }
+            .clip(tooltipShape)
             .border(
                 width = 1.dp,
                 color = ProtonTheme.colors.borderNorm,
