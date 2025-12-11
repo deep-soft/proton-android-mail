95767bb66e6cfedea19d24241687d791ffd8cdfd
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-12-11 09:48:20 +0100

    Add edgeToEdge to LockScreenActivity
    ET-5577

--- app/src/main/kotlin/ch/protonmail/android/feature/lockscreen/LockScreenActivity.kt
+++ app/src/main/kotlin/ch/protonmail/android/feature/lockscreen/LockScreenActivity.kt
@@ -23,6 +23,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -43,6 +44,7 @@ import io.sentry.compose.withSentryObservableEffect
 internal class LockScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         // Block back navigation
